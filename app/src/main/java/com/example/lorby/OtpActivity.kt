51cafca18bcca75b6b0d28a40e0e3c0f30354478
abdykadyr <abdukadyrabdilazov@icloud.com>
2024-03-20 14:33:37 +0100

    almost done

--- app/src/main/java/com/example/lorby/OtpActivity.kt
+++ app/src/main/java/com/example/lorby/OtpActivity.kt
@@ -1,12 +1,15 @@
 package com.example.lorby
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import com.example.lorby.databinding.ActivityOtpBinding
 import com.google.firebase.auth.FirebaseAuth
-import java.util.Random
+import papaya.`in`.sendmail.SendMail
+import kotlin.random.nextInt
+
 
 class OtpActivity : AppCompatActivity() {
 
@@ -14,6 +17,7 @@ class OtpActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
     var email: String = ""
     var password: String = ""
+    var random: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,33 +26,42 @@ class OtpActivity : AppCompatActivity() {
         email = intent.getStringExtra("email").toString()
         password = intent.getStringExtra("password").toString()
 
-        binding.etFirst.doOnTextChanged{text, start, before, count ->
-            if (!binding.etFirst.text.toString().isEmpty()){
+        auth = FirebaseAuth.getInstance()
+
+        random()
+
+
+        binding.tvResendOtp.setOnClickListener {
+            random()
+        }
+
+        binding.etFirst.doOnTextChanged { text, start, before, count ->
+            if (!binding.etFirst.text.toString().isEmpty()) {
                 binding.etSecond.requestFocus()
             }
-            if (!binding.etSecond.text.toString().isEmpty()){
+            if (!binding.etSecond.text.toString().isEmpty()) {
                 binding.etSecond.requestFocus()
             }
         }
-        binding.etSecond.doOnTextChanged{text, start, before, count ->
-            if (!binding.etSecond.text.toString().isEmpty()){
+        binding.etSecond.doOnTextChanged { text, start, before, count ->
+            if (!binding.etSecond.text.toString().isEmpty()) {
                 binding.etThird.requestFocus()
-            }else{
+            } else {
                 binding.etFirst.requestFocus()
             }
         }
-        binding.etThird.doOnTextChanged{text, start, before, count ->
-            if (!binding.etThird.text.toString().isEmpty()){
+        binding.etThird.doOnTextChanged { text, start, before, count ->
+            if (!binding.etThird.text.toString().isEmpty()) {
                 binding.etFourth.requestFocus()
-            }else{
+            } else {
                 binding.etSecond.requestFocus()
             }
         }
-        binding.etFourth.doOnTextChanged{text, start, before, count ->
-            if (!binding.etFourth.text.toString().isEmpty()){
+        binding.etFourth.doOnTextChanged { text, start, before, count ->
+            if (!binding.etFourth.text.toString().isEmpty()) {
 //                binding.etFirst.requestFocus()
             }
-            binding.btnDone.setOnClickListener{
+            binding.btnDone.setOnClickListener {
                 var otp1 = binding.etFirst.text.toString()
                 var otp2 = binding.etSecond.text.toString()
                 var otp3 = binding.etThird.text.toString()
@@ -56,21 +69,38 @@ class OtpActivity : AppCompatActivity() {
 
                 var otp = "$otp1$otp2$otp3$otp4"
 
-                if (binding.etFirst.text.toString().isEmpty()||
-                    binding.etSecond.text.toString().isEmpty()||
-                    binding.etThird.text.toString().isEmpty()||
-                    binding.etFourth.text.toString().isEmpty()){
+                if (binding.etFirst.text.toString().isEmpty() ||
+                    binding.etSecond.text.toString().isEmpty() ||
+                    binding.etThird.text.toString().isEmpty() ||
+                    binding.etFourth.text.toString().isEmpty()
+                ) {
                     Toast.makeText(this@OtpActivity, "Enter code", Toast.LENGTH_SHORT).show()
-                }
-//                else if(!otp.equals())
-                          {
-
+                } else if (!otp.equals(random.toString())) {
+                    Toast.makeText(this@OtpActivity, "Wrong code", Toast.LENGTH_SHORT).show()
+                } else {
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            var intent = Intent(this@OtpActivity, HomeScreen::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(
+                                this@OtpActivity,
+                                it.exception?.message.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
                 }
             }
-
         }
-//        fun random(){
-//            random=Random.nextInt(100000..999999)
-//        }
+    }
+
+    fun random() {
+        random = kotlin.random.Random.nextInt(1000..9999)
+        val mail = SendMail(
+            "aabdilazov10@gmail.com", "mwtupeqwomjczbba", email, "Login Sign up app's",
+            "Your code is -> $random"
+        )
+        mail.execute()
     }
 }
